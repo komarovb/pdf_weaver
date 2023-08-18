a027e684324214e2195b31e780d7ff143a5890f8
Borys Komarov <borys@Boryss-MacBook-Pro.local>
2023-08-17 23:11:23 -0400

    Initial GUI design implemented

--- pdf_weaver.rb
+++ pdf_weaver.rb
@@ -60,10 +60,30 @@ module PDFWeaver
   class WeaverGUI
     include Glimmer
   
+    BasicWeaverFile = Struct.new(:selected, :filename, :filepath)
+    class WeaverFile < BasicWeaverFile
+      def action
+        'Remove'
+      end
+    end
+
+    attr_accessor :weaver_files
+
     def initialize
       @arguments = {output_file: "./output.pdf", input_files: []}
       @running = false
       @worker = nil
+      @file_selection_element = nil
+      @weaver_files = [
+        WeaverFile.new(true, "file1.pdf", "/home/borys/file1.pdf")
+      ]
+      @instructions_header = %{How to use PDF Weaver\n\n}
+      @instructions = %{Use the following instructions to merge multiple PDF files\n
+1. Click the "Select files" button on the right side\n   
+2. Pick files that you would like to merge\n
+3. Confirm your choice and / or add more files int the next step\n
+4. Click Merge to merge or Cancel to clear the list of files\n
+5. Find merged file in the specified output directory\n}
     end
   
     def launch
@@ -72,12 +92,80 @@ module PDFWeaver
     end
   
     def create_gui
-      @main_window = window('PDF Weaver', 600, 400, true) {
+      @main_window = window('PDF Weaver', 600, 500, true) {
         margined true
   
         vertical_box {
           horizontal_box {
-            label("PDF Weaver")
+            # Instructions text
+            area {
+              stretchy true
+              text {
+                align :center
+                default_font family: 'Courier New', size: 14, weight: :regular, stretch: :normal
+                string {
+                  font family: 'Courier New', size: 22, weight: :bold, stretch: :normal
+                  @instructions_header
+                }
+                string {
+                  @instructions
+                }
+              }
+            }
+          }
+          horizontal_box {
+            vertical_box {
+              button("Select files") {
+                stretchy false
+                
+                on_clicked do
+                  file = open_file
+                  unless file.nil?
+                    puts "Selected #{file}"
+                    if File.exist?(file)
+                      @weaver_files << WeaverFile.new(true, File.basename(file), File.dirname(file))
+                    end
+                  end
+                  $stdout.flush # for Windows
+
+                end
+              }
+              button("Select folder") {
+                stretchy false
+                
+                on_clicked do
+                  file = open_folder
+                  puts file unless file.nil?
+                  $stdout.flush # for Windows
+                end
+              }
+              table {
+                checkbox_column('Selected') {
+                  editable true
+                }
+                text_column('Filename')
+                button_column('Action') {
+                  on_clicked do |row_id|
+                    @weaver_files.delete_at(row_id)
+                  end
+                }
+          
+                editable false
+                cell_rows <=> [self, :weaver_files] # explicit data-binding to self.contacts Model Array, auto-inferring model attribute names from underscored table column names by convention
+                
+                on_changed do |row, type, row_data|
+                  puts "Row #{row} #{type}: #{row_data}"
+                  $stdout.flush # for Windows
+                end
+              }
+              button("Merge selected files") {
+                stretchy false
+                
+                on_clicked do
+                  puts "Merging!"
+                end
+              }      
+            }
           }
         }
       }
@@ -91,7 +179,7 @@ if __FILE__ == $0
     # status = cli_interface.do_work
     # exit(status)
     PDFWeaver::WeaverGUI.new.launch
-  resque => ex
+  rescue => ex
     puts "Failing with an exception! #{ex.message}"
     exit(1)
   end
